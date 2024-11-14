b0137169f47bd7be6abab36062be19c9bec1035f
Rishith <nallapurishithreddy@gmail.com>
2024-11-14 15:31:17 +0530

    hdb

---
+++ src/main/java/maven_java/mavenjava/App.java
@@ -0,0 +1,10 @@
+package maven_java.mavenjava;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Rishith Reddy - 22BD1A053D");
+    }
+}
